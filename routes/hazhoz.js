f52f1bf82a49a7f8d9d688e87ab22c89b3ea865c
reklam11 <reklam11@gmail.com>
2020-03-22 13:17:59 +0000

    Installed ejs, added hazhoz route

---
+++ routes/hazhoz.js
@@ -0,0 +1,5 @@
+router.get('/hazhoz', function(req, res, next) {
+  res.send("Hazhozszallitas!");
+});
+
+module.exports = router;
--- routes/routes.js
+++ routes/routes.js
@@ -6,4 +6,12 @@ router.get('/', function(req, res, next) {
   res.send("It works!");
 });
 
+router.get('/hazhoz', function(req, res, next) {
+  res.render("hazhoz.ejs");
+});
+
+router.get('/hazhoz', function(req, res, next) {
+  res.send("Hazhozszallitas!");
+});
+
 module.exports = router;
