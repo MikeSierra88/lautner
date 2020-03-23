52192c1326c76c07e6472080a6ce057e4813ff83
EC2 Default User <ec2-user@ip-172-31-33-35.eu-central-1.compute.internal>
2020-03-23 15:32:18 +0000

    Added basic post route for /hazhoz, basic dynamic data display using dummy data

--- routes/routes.js
+++ routes/routes.js
@@ -6,8 +6,38 @@ router.get('/', function(req, res, next) {
   res.redirect('hazhoz');
 });
 
+var termekek = [
+  {nev: "Minyon", kategoria: "sutemeny", kiszereles: "darab", ar:"250 Ft"},
+  {nev: "Kardinalis", kategoria: "sutemeny", kiszereles: "darab", ar:"350 Ft"},
+  {nev: "Teasutemeny", kategoria: "sutemeny", kiszereles: "ledig", ar:"3000 Ft/kg"}
+]
+
+var kategoriak = [
+  {kat: "sutemeny"}  
+]
+
 router.get('/hazhoz', function(req, res, next) {
-  res.render("hazhoz.ejs");
+
+  var kategoriak = [
+    {kat: "sutemeny"}  
+  ]
+  
+  res.render("hazhoz.ejs", {termekek:termekek, kategoriak:kategoriak});
+});
+
+router.post('/hazhoz', function (req, res, next) {
+  var nev = req.body.nev;
+  var kategoria = req.body.kategoria;
+  var kiszereles = req.body.kiszereles;
+  var ar = req.body.ar;
+  var newSuti = {nev: nev, kategoria: kategoria, kiszereles: kiszereles, ar: ar};
+  termekek.push(newSuti);
+  res.redirect('/hazhoz');
+  next();
+});
+
+router.get('/hazhoz/new', function(req, res, next){
+  res.render('new.ejs')
 });
 
 router.get('/husvet', function(req, res, next) {
