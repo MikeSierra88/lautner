62c04a9f6a2a82ff4f4105f0c118e2ec85bc087d
EC2 Default User <ec2-user@ip-172-31-33-35.eu-central-1.compute.internal>
2020-03-25 08:08:12 +0000

    Connection added to mongodb

--- app.js
+++ app.js
@@ -8,6 +8,50 @@ var indexRouter = require('./routes/routes');
 
 var app = express();
 
+// connect database using mongoose
+const LAUTNERDB_URI = process.env.LAUTNERDB + '/test?retryWrites=true&w=majority';
+var mongoose = require('mongoose');
+mongoose.set('useUnifiedTopology', true);
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+
+
+mongoose.connect(LAUTNERDB_URI).
+  catch(error => function(err){
+     console.log(err.reason);
+  });
+  
+var db = mongoose.connection;
+db.once('open', function() {
+  console.log('DB connected');
+  // var kittySchema = new mongoose.Schema({
+  //   name: String
+  // }, {writeConcern: {w: 1}});
+  
+  // kittySchema.methods.speak = function () {
+  //   var greeting = this.name
+  //     ? "Meow name is " + this.name
+  //     : "I don't have a name";
+  //   console.log(greeting);
+  // }
+  
+  // var Kitten = mongoose.model('Kitten', kittySchema);
+  
+  // var fluffy = new Kitten({ name: 'fluffy' });
+  // fluffy.speak(); // "Meow name is fluffy"
+  
+  // fluffy.save(function (err, fluffy) {
+  //   if (err) return console.error(err);
+  //   fluffy.speak();
+  // });
+  
+  // Kitten.find(function (err, kittens) {
+  //   if (err) return console.error(err);
+  //   console.log(kittens);
+  // })
+});
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
