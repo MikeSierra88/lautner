44d15367a80a5ba391f1ecb2f4a791c945813102
MikeSierra88 <reklam11@gmail.com>
2021-12-30 14:18:16 +0000

    Static changes

--- public/javascript/l.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function(){
-    $('.nav-item.active').removeClass('active');
-    $('.nav-item a[href="' + location.pathname  + '"]').parent().addClass('active');
-    $(this).parent().addClass('active').siblings().removeClass('active');
-});
--- routes/routes.js
+++ routes/routes.js
@@ -3,16 +3,36 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.redirect('hazhoz');
+  res.render("index.ejs");
 });
 
-router.get('/hazhoz', function(req, res, next) {
-  res.render("hazhoz.ejs");
+router.get('/kapcsolat', function(req, res, next) {
+  res.render("kapcsolat.ejs");
 });
 
-router.get('/husvet', function(req, res, next) {
-  res.render("husvet.ejs");
+router.get('/rendezveny', function(req, res, next) {
+  res.render("event.ejs");
 });
+
+router.get('/viszontelado', function(req, res, next) {
+  res.render("retailer.ejs");
+});
+
+router.get('/rolunk', function(req, res, next) {
+  res.render("rolunk.ejs");
+});
+
+router.get('/kinalat', function(req, res, next) {
+  res.render("kinalat.ejs");
+});
+
+// router.get('/karacsony', function(req, res, next) {
+//   res.render("kinalat-karacsony.ejs");
+// });
+
+// router.get('/husvet', function(req, res, next) {
+//   res.redirect("/kinalat");
+// });
 
 router.get('*', function(req, res, next) {
   res.redirect('/');
